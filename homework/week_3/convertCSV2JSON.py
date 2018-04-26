102e8bd2b82d49bf4c6371cb82f6a8440b5c024c
fietsboekstoelgit config --global user.name fietsboekstoelgit commit -m first <fietsboekstoel>
2018-04-26 14:52:20 +0200

    bijna helemaal af

--- homework/week_3/convertCSV2JSON.py
+++ homework/week_3/convertCSV2JSON.py
@@ -1,4 +1,14 @@
-# HEADER MAKEN
+#
+# convertcsvtojson.py
+#
+# Rebecca de Feijter - 10639918
+# Data Processing Week 3 D3 Barchart
+#
+# Converts a csv file to json format.
+#
+# If csvfile lacks fieldnames, behind 'csvfile' in line 19 add something like:
+# , fieldnames = ( "fieldname0","fieldname1","fieldname2","fieldname3" )
+#
 
 import csv
 import json
